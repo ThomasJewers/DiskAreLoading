355db92da757aa49e3c0364d1087a93e09eee8bd
ThomasJ <114022075+ThomasJewers@users.noreply.github.com>
2023-01-28 19:33:40 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        //object instantiation
         Circle circle1 = new Circle();
         Circle circle2 = new Circle(0.4);
         Circle circle3 = new Circle(0.6, "Red");
+        //printing
         System.out.println(circle1.getColour());
         System.out.println(circle1.getRadius());
         System.out.println(circle2);
